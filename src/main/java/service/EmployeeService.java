c7c268f68abde2fd61fc5c528b585eb23539cbc2
Samia Jahan <samia.jahan@revature.net>
2022-03-18 15:44:27 -0400

    CI CD testing

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -9,19 +9,19 @@ import pojo.MergedReimbursmentPojo;
 
 public interface EmployeeService {
 	EmployeePojo employeeInfo(int employeeId) throws SystemException;
-//	  View their information. ------fetchAnEmployee
+
 	EmployeePojo employeeViewDetails(String employeeContact)throws SystemException;
-//	Login. 
+ 
 	EmployeePojo login(String employeeContact, String employeePassword)throws SystemException;	
-//	 Submit a reimbursement request.
+
 	MergedReimbursmentPojo submitReimbursementReq(MergedReimbursmentPojo pendingReimbursementPojo)throws SystemException;
-//	imp• View their pending reimbursement requests. 
+ 
 	
 	MergedReimbursmentPojo viewReimbursementPendingReq(int reimbursementId)throws SystemException;
-//	imp• View their resolved reimbursement requests.
+
 	MergedReimbursmentPojo viewReimbursementResolveReq(int reimbursementId)throws SystemException;
 		
-//	• Update their information.-----update 
+
 	
 	EmployeePojo updateEmployeeInfo(EmployeePojo employeePojo)throws SystemException;
 
--- src/main/java/service/ManagerService.java
+++ src/main/java/service/ManagerService.java
@@ -10,27 +10,27 @@ import pojo.MergedReimbursmentPojo;
 
 public interface ManagerService {
 
-	  //fetch a manager
+	  
 		ManagerPojo fetchOneManager(String employeeContact)throws SystemException;
 		
 		ManagerPojo login(String managerContact, String managerPassword)throws SystemException;
 		ManagerPojo managerViewinfo(String managerContact)throws SystemException;
 		
 	 
-		// add/delete in two Reimbursement table
+		
 		MergedReimbursmentPojo approveOrDeny(MergedReimbursmentPojo resolvedReimbursmentPojo)throws SystemException;
 		
-		//Fetch/read a pending request
+	
 		MergedReimbursmentPojo fetchAPendingRequest(int reimbursementId) throws SystemException;
-//		imp• View all pending requests of all employees. 
+ 
 		List<MergedReimbursmentPojo> fetchAllPendingReq()throws SystemException;
-//		imp• View all resolved requests of all employees.
+
 		 List<MergedReimbursmentPojo> fetchAllResolveReq()throws SystemException;
 		
-		//• View reimbursement requests of a specific employee.
+		
 		 List<MergedReimbursmentPojo> viewReimbursementReq(int requestingEmployeeId)throws SystemException;
 		
-		//read
+		
 			List<EmployeePojo> fetchAllEmployee()throws SystemException;
 	
 	
